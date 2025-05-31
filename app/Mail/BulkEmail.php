7aab89180d493c8a31219201fe9f768179dc9e62
itafor <itaforfrancis@gmail.com>
2025-05-31 21:07:47 +0100

    updated

---
+++ app/Mail/BulkEmail.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class BulkEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $details;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($details)
+    {
+        $this->details = $details;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: 'Bulk Email',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            markdown: 'emails.bulk',
+        );
+    }
+
+    public function build()
+    {
+        return $this->subject($this->details['subject'])
+            ->markdown('emails.bulk')
+            ->with('details', $this->details);
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
